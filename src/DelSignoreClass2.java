142c9e47a33ddb369a7f4b311004d55d4cc4ee46
anthonydelsignore <adelsignore@student.bridgew.efu>
2021-09-13 14:50:42 -0400

    9/13 "here are the students in your class"

---
+++ src/DelSignoreClass2.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+//make sure .txt is in main project folder or it won't work
+public class DelSignoreClass2 {
+    public static  void main(String[] args) throws IOException{
+        var filename = "student.txt";
+        var studentFile= Paths.get(filename);
+        var allStudents = Files.readString(studentFile);
+        String [] seperatedNames = allStudents.split("\n");
+        //now lets show the user
+        System.out.println("Here are the students in your class:");
+        for(var studentName: seperatedNames){
+            System.out.println(studentName);
+        }
+
+
+    }
+
+}
